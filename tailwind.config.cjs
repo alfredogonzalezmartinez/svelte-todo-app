21fd9905fbb2b29b145b36dded62a3b222dcdd35
Alfredo González Martínez <48945157+alfredogonzalezmartinez@users.noreply.github.com>
2023-08-05 20:09:33 +0200

    Add custom tailwind xs breakpoint

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -1,8 +1,14 @@
+const defaultTheme = require('tailwindcss/defaultTheme')
+
 /** @type {import('tailwindcss').Config}*/
 const config = {
 	content: ['./src/**/*.{html,js,svelte,ts}'],
 
 	theme: {
+		screens: {
+			xs: '475px',
+			...defaultTheme.screens,
+		},
 		extend: {},
 	},
 
